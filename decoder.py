e28d8459f6fca22418ac82f05088068b38f93f36
TunayAdaKaracan <tunayada@gmail.com>
2026-03-05 19:02:56 +0100

    initial

---
+++ decoder.py
@@ -0,0 +1,97 @@
+# pyright: reportInvalidTypeForm=false
+
+from isa import OpCodes
+from test_helpers.decoder_test_codes import code_1
+
+from amaranth import *
+from amaranth.sim import Period, Simulator
+from amaranth.lib import wiring
+
+
+class Decoder(wiring.Component):
+    # Inputs
+    instruction:    wiring.In(32)
+
+    # Outputs Values
+    rs1:            wiring.Out(5)
+    rs2:            wiring.Out(5)
+    rd:             wiring.Out(5)
+
+    funct7:         wiring.Out(7)
+    funct3:         wiring.Out(3)
+
+    immediate:      wiring.Out(32)
+
+    # Output Signals
+    lui:            wiring.Out(1)
+    auipc:          wiring.Out(1)
+
+    jal:            wiring.Out(1)
+    jalr:           wiring.Out(1)
+
+    branch:         wiring.Out(1)
+    load:           wiring.Out(1)
+    store:          wiring.Out(1)
+
+    alu_op_i:       wiring.Out(1)
+    alu_op_r:       wiring.Out(1)
+
+    fence:          wiring.Out(1)
+    system:         wiring.Out(1)
+
+    def __init__(self):
+        super().__init__()
+    
+    def elaborate(self, platform):
+        m = Module()
+
+        opcode = Signal(7)
+        m.d.comb += opcode.eq(self.instruction[0:7])
+
+        m.d.comb += [
+            self.lui.eq(opcode      == OpCodes.LUI),
+            self.auipc.eq(opcode    == OpCodes.AUIPC),
+            self.jal.eq(opcode      == OpCodes.JAL),
+            self.jalr.eq(opcode     == OpCodes.JALR),
+            self.branch.eq(opcode   == OpCodes.BRANCH),
+            self.load.eq(opcode     == OpCodes.LOAD),
+            self.store.eq(opcode    == OpCodes.STORE),
+            self.alu_op_i.eq(opcode == OpCodes.ALU_I),
+            self.alu_op_r.eq(opcode == OpCodes.ALU_R),
+            self.fence.eq(opcode    == OpCodes.FENCE),
+            self.system.eq(opcode   == OpCodes.SYSTEM)
+        ]
+
+        m.d.comb += [
+            self.rs1.eq(self.instruction[15:20]),
+            self.rs2.eq(self.instruction[20:25]),
+            self.rd.eq(self.instruction[7:12])
+        ]
+
+        m.d.comb += [
+            self.funct3.eq(self.instruction[12:15]),
+            self.funct7.eq(self.instruction[25:32])
+        ]
+
+        # TODO: Immediate decoding
+        m.d.comb += self.immediate.eq(0)
+        return m
+
+def test():
+    top = Decoder()
+
+    async def bench(ctx):
+        for instr in code_1:
+            print(f"Emulating instruction {instr}")
+            ctx.set(top.instruction, int(instr, base=2))
+            await ctx.delay(Period(us=2))
+    
+
+    sim = Simulator(top)
+    sim.add_testbench(bench)
+
+    with sim.write_vcd("sim_output/decoder.vcd"):
+        sim.run()
+
+if __name__ == "__main__":
+    test()
---
+++ isa.py
@@ -0,0 +1,86 @@
+# Instruction Types:
+
+# R-Type: rd = rs1 OP rs2
+# funct7[7]     | rs2[5] | rs1[5] | funct3[3] | rd[5]       | opcode[7]
+
+# I-Type: rd = rs1 OP Imm(12) | Load Op
+# Imm[11:0]              | rs1[5] | funct3[3] | rd[5]       | opcode[7]
+
+# S-Type: 
+# Imm[11:5]     | rs2[5] | rs1[5] | funct3[3] | Imm[4:0]    | opcode[7]
+
+# B-Type:
+# Imm[12|10:5]  | rs2[5] | rs1[5] | funct3[3] | Imm[4:1|11] | opcode[7]
+
+# U-Type:
+# Imm[31:12]                                  | rd[5]       | opcode[7]
+
+# J-Type:
+# Imm[20|10:1|11|19:12]                       | rd[5]       | opcode[7]
+
+class OpCodes:
+    LUI     = 0b0110111 # U-Type -> Load Upper Immediate
+    AUIPC   = 0b0010111 # U-Type -> Add Upper Immediate to PC
+
+    JAL     = 0b1101111 # J-Type -> Jump and Link
+    JALR    = 0b1100111 # I-Type -> Jump and Link Register
+
+    # Branch Operations (0b1100011) | B-Type
+    BRANCH  = 0b1100011 # Grouping
+    BEQ     = 0b1100011
+    BNE     = 0b1100011
+    BLT     = 0b1100011
+    BGE     = 0b1100011
+    BLTU    = 0b1100011
+    BGEU    = 0b1100011
+
+    # Load Operations   (0b0000011) | I-Type
+    LOAD    = 0b0000011 # Grouping
+    LB      = 0b0000011
+    LH      = 0b0000011
+    LW      = 0b0000011
+    LBU     = 0b0000011
+    LHU     = 0b0000011
+
+    # Store Operations  (0b0100011) | S-Type
+    STORE   = 0b0100011 # Grouping
+    SB      = 0b0100011
+    SH      = 0b0100011
+    SW      = 0b0100011
+
+    # ALU Operations:
+    # opcode[5] == 0b1 -> Immediate Mode
+    # opcode[5] == 0b0 -> Register  Mode
+
+    # ALU-I Operations  (0b0010011) | I-Type
+    ALU_I   = 0b0010011 # Grouping
+    ADDI    = 0b0010011
+    SLTI    = 0b0010011
+    SLTIU   = 0b0010011
+    XORI    = 0b0010011
+    ORI     = 0b0010011
+    ANDI    = 0b0010011
+    SLLI    = 0b0010011
+    SRLI    = 0b0010011
+    SRAI    = 0b0010011
+
+    # ALU-R Operations  (0b0110011) | R-Type
+    ALU_R   = 0b0110011 # Grouping
+    ADD     = 0b0110011
+    SUB     = 0b0110011
+    SLL     = 0b0110011
+    SLT     = 0b0110011
+    SLTU    = 0b0110011
+    XOR     = 0b0110011
+    SRL     = 0b0110011
+    SRA     = 0b0110011
+    OR      = 0b0110011
+    AND     = 0b0110011
+
+    # Fence Operaton    (0b0001111) | I-Type
+    FENCE   = 0b0001111
+
+    # System Operations (0b1110011)
+    SYSTEM  = 0b1110011 # Grouping
+    ECALL   = 0b1110011
+    EBREAK  = 0b1110011
